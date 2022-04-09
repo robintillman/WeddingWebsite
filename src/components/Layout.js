d60f5c0fe7c5ccbf8d6abda887d630f39ba2959e
Robin Tillman <robin.tillman92@gmail.com>
2022-04-09 15:20:09 +0200

    fix

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,19 +5,13 @@ import Footer from './Footer';
 import backgroundImage from '../resources/images/backgroundimage.jpg';
 
 const styles = makeStyles({
-    layout: {
-        height: '100%'
-    },
     mainContentContainer: {
-        height: '100%',
         backgroundImage: `url(${backgroundImage})`,
         backgroundPosition: 'center', 
         backgroundSize: 'cover', 
         backgroundRepeat: 'no-repeat'
       },
     mainContent: {
-        flexDirection: "column",
-        alignItems: "center",
         padding: "3rem",
         marginTop: "1rem",
         marginBottom: "1rem"
@@ -28,12 +22,12 @@ function Layout({ children }) {
     const classes = styles();
 
     return (
-        <div className={classes.layout}>
+        <div>
             {/* Navigation */}
             <NavBar />
 
             {/* Main content */}
-            <Grid className={classes.mainContentContainer} container>
+            <Grid container className={classes.mainContentContainer}>
                 <Grid item xs={1} />
                 <Grid item xs={10}>
                     <Paper className={classes.mainContent}>
